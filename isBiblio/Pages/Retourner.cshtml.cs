5101f43ecf4e6fc850ab792946dae39aac53fb45
Issouf SORGHO <mastersorgho226@gmail.com>
2024-11-01 12:52:35 -0400

    Gestion de retour

---
+++ isBiblio/Pages/Retourner.cshtml.cs
@@ -0,0 +1,61 @@
+using iSBiblio.Data;
+using iSBiblio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+
+namespace iSBiblio.Pages
+{
+    public class RetournerModel : PageModel
+    {
+        private readonly BibliothequeContext _context;
+
+        public RetournerModel(BibliothequeContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty(SupportsGet = true)] // Permet de lier l'ID lors d'une requête GET
+        public int EmpruntID { get; set; }
+
+
+
+        public Emprunt Emprunt { get; set; }
+        public Utilisateur utilisateur { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            EmpruntID = id;
+            var userEmail = User.FindFirstValue(ClaimTypes.Name);
+            utilisateur = _context.Utilisateurs.FirstOrDefault(u => u.Email == userEmail);
+
+
+            string con_str = "Server=isorgho;Database=Bibliotheque;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            using (var connection = new SqlConnection(con_str))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("RetournerLivre", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add(new SqlParameter("@EmpruntID", EmpruntID));
+                    int row = command.ExecuteNonQuery();
+
+                    if (row > 0)
+                    {
+                        TempData["Message"] = "Merci ! Vous avez retourné un livre avec succès !";
+                        return Redirect("/Emprunt");
+
+                    }
+                    else
+                    {
+                        return RedirectToPage("/MenuLivres");
+                    }
+                }
+            }
+
+        }
+    }
+}
